e6bf370a9cbee1dfaf784dd9e21387219e5b81ca
Mike Ackison <mike.ackison@shopify.com>
2021-01-14 23:11:09 +0000

    refactor with callback, module.export to seperate file

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,36 +3,40 @@ const request = require('request');
 // access node request librry
 // allow user to to specify the breed name using command-line arguments
 
-const userInput = process.argv.slice(2);
 
-request(
 
-  // if the breedname is not found it reutuns undefined
-  // if error is not null
+// move the main request logic into the fetchBreedDescription function
+// the callback should either return an error or null(if there isn't an error) 
+// if error is null
 
-  `https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, function(error, response, body) {
+
+const fetchBreedDescription = function(userInput, callback) {
+  request( `https://api.thecatapi.com/v1/breeds/search?q=${userInput}`, function(error, response, body) {
 
     if (!error) {
-      // if there is no error
-      
-      console.error('error:', error); // Print the error if one occurred
-      console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-      console.log('body:', body); // Print the HTML for the Google homepage.
+      // if there is no error/ if error is not null
+
+      // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+      // console.log('body:', body); // Print the body
       
       // deserialization
       const data = JSON.parse(body);
-      console.log("Datat.length", data.length);
+      // console.log("data.length", data.length);
       
-      if (data.length !== 0) {
-        // if the array is empty
-        // print  out first entry in the data array
-        console.log("first entry: weight:", data[0].weight);
+      if (data.length !== 0) { // if the array is not empty
+        // console.log("desc:", data[0].description);
+        callback(null, data[0].description);
       } else {
-        console.log("Requested breed not found");
+        // if the array is empty, breed not found
+        callback(null, "Requested breed not found.");
       }
     } else {
       // otherwise if there is an error
-      console.log("Request Failed");
+      // console.error('error:', error); // Print the error if one occurred
+      callback(error, null);
     }
   });
 
+};
+
+  module.exports = { fetchBreedDescription };
